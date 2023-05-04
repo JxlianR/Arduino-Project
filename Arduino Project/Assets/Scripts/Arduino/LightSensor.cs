75104faa1f0c918ec1937da574ba5d0df8edbecb
Meeeri08 <61042895+Meeeri08@users.noreply.github.com>
2023-05-05 00:53:05 +0200

    Game light sensor feedback

--- Arduino Project/Assets/Scripts/Arduino/LightSensor.cs
+++ Arduino Project/Assets/Scripts/Arduino/LightSensor.cs
@@ -14,6 +14,7 @@ public class LightSensor : MonoBehaviour
     public int minSeconds, maxSeconds;
 
     SpriteRenderer spriteRenderer;
+    TextMesh textMesh;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,15 @@ public class LightSensor : MonoBehaviour
         //sp.Open();
 
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        GameObject textObject = new GameObject("LightFeedback");
+        textObject.transform.parent = transform;
+
+        textMesh = textObject.AddComponent<TextMesh>();
+        textMesh.anchor = TextAnchor.MiddleCenter;
+        textMesh.alignment = TextAlignment.Center;
+        textMesh.characterSize = 0.5f; 
+
         StartCoroutine(ChangeLights());
     }
 
@@ -53,8 +63,20 @@ public class LightSensor : MonoBehaviour
     {
         int waitSeconds = UnityEngine.Random.Range(minSeconds, maxSeconds);
         yield return new WaitForSeconds(waitSeconds);
+
         spriteRenderer.enabled = true;
         lightsOut = true;
+
+        textMesh.text = "Oh no, lights went off.\nMaybe there's something I can do.";
+        Camera mainCamera = Camera.main;
+        Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+        Vector3 worldCenter = mainCamera.ScreenToWorldPoint(screenCenter);
+        worldCenter.z = -1f; 
+        textMesh.transform.position = worldCenter;
+
+         yield return new WaitForSeconds(3f);
+        textMesh.text = string.Empty;
+
     }
 
     void Close()
